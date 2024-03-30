e4e84935a185ee8f867eb53962245914eeec5a47
Kari <kari_martikainen@icloud.com>
2024-03-30 15:20:15 +0200

    Add blog login tests

---
+++ tests/blog-login.spec.js
@@ -0,0 +1,36 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test');
+
+describe('Blog app', () => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3003/api/testing/reset');
+    await request.post('http://localhost:3003/api/login', {
+      data: {
+        username: 'testaaja',
+        password: 'testinen'
+      }
+    });
+  });
+
+  test('Login form is shown', async ({ page }) => {
+    await page.goto('http://localhost:5173');
+    await page.waitForSelector('form');
+  });
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.fill('#username', 'testaaja');
+      await page.fill('#password', 'testinen');
+      await page.click('text=login');
+      await page.waitForSelector('.success');
+    });
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.fill('#username', 'testaaja');
+      await page.fill('#password', 'wrong'); // Incorrect password
+      await page.click('text=login');
+      await page.waitForSelector('.error');
+      const error = await page.$('.error');
+      expect(error).not.toBeNull();
+    });
+  });
+});
